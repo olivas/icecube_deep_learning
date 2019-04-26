563e1e7d9c09d708d50e4ab9e1b2f0b7a5b4a8e5
Alex Olivas <olivas@icecube.umd.edu>
2019-04-26 16:04:11 -0600

    this works

--- single_hidden_layer.py
+++ single_hidden_layer.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
+import os
 import argparse
 import logging
+import pickle
+import random
+import numpy as np
+import tensorflow as tf
 
 parser = argparse.ArgumentParser(description='Hello IceCube')
+parser.add_argument('--data_path',
+                    default='/opt/icecube_deep_learning/data',
+                    help='Path to training and test data.')
+parser.add_argument('--epochs',
+                    dest='epochs',
+                    default=10,
+                    type=int,
+                    help='Number of units in the hidden layer.')
 parser.add_argument('--n-hidden-units',
                     dest='n_hidden_units',
-                    default=128,
+                    default=512,
+                    type=int,
                     help='Number of units in the hidden layer.')
 parser.add_argument('--activation',
                     dest='activation',
@@ -18,23 +32,24 @@ parser.add_argument('--optimizer',
                     help='Name of tf.keras.optimizer function.')
 parser.add_argument('--loss',
                     dest='loss',
-                    default='log_loss',
+                    default='sparse_categorical_crossentropy',
                     help='Name of tf.keras.loss function.')
 parser.add_argument('--metrics',
                     dest='metrics',
-                    default='metrics',
+                    default='accuracy',
                     help='Name of tf.keras.metrics function.')
 args = parser.parse_args()
 
-activtion_options = ['elu', 'hard_sigmoid', 'linear', 'relu', 'selu',
+activation_options = ['elu', 'hard_sigmoid', 'linear', 'relu', 'selu',
                      'sigmoid', 'softmax', 'softplus', 'softsign', 'tanh']
 
 optimizer_options = ['adadelta', 'adagrad', 'adam', 'adamax', 'nadam', 'rmsprop', 'sgd']
 
-loss_options = ['absolute_difference', 'cosine_distance', 'hinge_loss',
-                'huber_loss', 'log_loss', 'mean_pairwise_squared_error',
-                'mean_squared_error', 'sigmoid_cross_entropy',
-                'softmax_cross_entropy', 'sparse_softmax_cross_entropy']
+loss_options = ['binary_crossentropy', 'categorical_hinge', 'cosine', 'cosine_proximity',
+                'hinge', 'kullback_leibler_divergence', 'logcosh', 'mae', 'mape',
+                'mean_absolute_error', 'mean_absolute_percentage_error', 'categorical_crossentropy',
+                'mean_squared_logarithmic_error', 'mse', 'poisson', 'sparse_categorical_crossentropy',
+                'squared_hinge']                
 
 metrics_options = ['accuracy', 'auc', 'average_precision_at_k', 'false_negatives',
                    'false_negatives_at_thresholds', 'false_positives', 'false_positives_at_thresholds',
@@ -46,6 +61,12 @@ metrics_options = ['accuracy', 'auc', 'average_precision_at_k', 'false_negatives
                    'specificity_at_sensitivity', 'true_negatives', 'true_negatives_ath_thresholds',
                    'true_positives', 'true_positives_at_thresholds']
 
+print(args.activation)
+print(args.optimizer)
+print(args.loss)
+
+print(loss_options)
+
 assert(args.activation in activation_options)
 assert(args.optimizer in optimizer_options)
 assert(args.loss in loss_options)
@@ -55,33 +76,42 @@ for m in args.metrics.split(','):
     if m in metrics_options:
         valid_metrics.append(m)
     else:        
-        logging.warn('%s is not a valid metric.  Ignoring.')
+        logging.warn('%s is not a valid metric.  Ignoring.' % m)
 
-import tensorflow
-import pickle
-import numpy
+tracks = pickle.load(open(os.path.join(args.data_path,'tracks.pkl')))
+cascades = pickle.load(open(os.path.join(args.data_path,'cascades.pkl')))
 
-data = pickle.load(open('bootcamp.pkl'))
+labeled_data = [(0,h) for h in cascades] + [(1,h) for h in tracks]
+random.shuffle(labeled_data)
 
-training_data = data['training_data']
-test_data = data['test_data']
+n_output_units = 2
 
-n_output_units = len(set(training_data['labels']))
+print(n_output_units)
 
 # Q1: How do I choose the number of units of the hidden layer?
 # Q2: What's the best activation function to use?
+input_layer = tf.keras.layers.Flatten(input_shape=(28,28))
 hidden_layer = tf.keras.layers.Dense(args.n_hidden_units, activation=args.activation)
-output_layer = tf.keras.layers.Dense(3)
+dropout_layer = tf.keras.layers.Dropout(0.2)
+output_layer = tf.keras.layers.Dense(n_output_units, activation='softmax')
 
-# Start with single
-network = [hidden_layer, output_layer]
+# Start with single hidden layer
+network = [input_layer, hidden_layer, dropout_layer, output_layer]
 model = tf.keras.models.Sequential(network)
 
+# Q3: What's the best optimizer?
+# Q4: What's the best loss function?
 model.compile(optimizer=args.optimizer,
               loss=args.loss,
               metrics=valid_metrics)
 
-model.fit(x_train, y_train, epochs=5)
+x_train = np.array([h for l,h in labeled_data[:100]])
+y_train = np.array([l for l,h in labeled_data[:100]])
+assert(len(x_train) == len(y_train))
+model.fit(x_train, y_train, epochs=args.epochs)
+
+x_test = np.array([h for l,h in labeled_data[100:]])
+y_test = np.array([l for l,h in labeled_data[100:]])
 model.evaluate(x_test, y_test)
 
 
